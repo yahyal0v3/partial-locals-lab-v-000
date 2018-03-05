83112b8e2798a56cef32bb55af821307f1d3f79c
Khaya Ramcharita-Dillon <yahyal0v3@yahoo.com>
2018-03-05 04:10:23 +0000

    automatically backed up by learn

--- app/models/student.rb
+++ app/models/student.rb
@@ -13,4 +13,15 @@
 class Student < ActiveRecord::Base
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
+
+  def self.search(name)
+    if name == ""
+      self.all
+    else
+      self.all.select do |student|
+        student.name.downcase.include?(name)
+      end
+    end
+  end
+
 end
